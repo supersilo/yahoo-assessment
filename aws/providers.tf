07226315f865dae5f44d7b2031709b6522923616
Azeez Adeniyi <supersilo4u@gmail.com>
2023-12-07 11:07:49 -0600

    Terraform fmt

--- aws/providers.tf
+++ aws/providers.tf
@@ -8,15 +8,15 @@ terraform {
 }
 
 provider "aws" {
-  alias = "default"
-  region = var.aws_default_region
-  access_key =  "<YOUR_AWS_ACCESS_KEY>"
+  alias      = "default"
+  region     = var.aws_default_region
+  access_key = "<YOUR_AWS_ACCESS_KEY>"
   secret_key = "<YOUR_AWS_SECRET_KEY>"
 }
 
 provider "aws" {
-  alias  = "east"
-  region = "us-east-1"
+  alias      = "east"
+  region     = "us-east-1"
   access_key = "<YOUR_AWS_ACCESS_KEY>"
   secret_key = "<YOUR_AWS_SECRET_KEY>"
 }
